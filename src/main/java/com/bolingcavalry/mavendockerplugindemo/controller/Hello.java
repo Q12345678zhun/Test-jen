cf1e2bfa8f4eb620f2f34315493756e67cf951ed
yuan <1401096173@qq.com>
2019-07-22 15:11:58 +0800

    修改返返

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -1,4 +1,4 @@
-package com.bolingcavalry.mavendockerplugindemo.controller;
+﻿package com.bolingcavalry.mavendockerplugindemo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +11,6 @@ public class Hello {
     @RequestMapping("/")
     public String sayHello(){
     	//修改返回的字符串的内容
-        return "123456. Hello jenkins, " + new Date();
+        return "abcdef. Hello jenkins, " + new Date();
     }
 }
